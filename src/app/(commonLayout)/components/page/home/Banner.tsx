7baf16c1c356ba625fefbf9e30e606c4c5bff8f5
mehedi-imun <mehediimun@gmail.com>
2025-01-17 01:37:33 +0600

    solve banner responsive issyue

--- src/app/(commonLayout)/components/page/home/Banner.tsx
+++ src/app/(commonLayout)/components/page/home/Banner.tsx
@@ -1,31 +1,45 @@
 import { Carousel } from "antd";
-import {
-  default as img1,
-  default as img6,
-} from "../../../../../../public/carousel/img1.png";
+import Image from "next/image";
+import img1 from "../../../../../../public/carousel/img1.png";
 import img2 from "../../../../../../public/carousel/img2.png";
 import img3 from "../../../../../../public/carousel/img3.png";
 import img4 from "../../../../../../public/carousel/img4.png";
 import img5 from "../../../../../../public/carousel/img5.png";
+import img6 from "../../../../../../public/carousel/img6.png";
 
 export default function Banner() {
-  // Array of images
   const images = [img1, img2, img3, img4, img5, img6];
 
   return (
-    <div style={{ width: "80%", margin: "auto", marginTop: "50px" }}>
-      <Carousel autoplay>
+    <div style={{ width: "80%", margin: "auto", marginTop: "50px", }}>
+      <Carousel adaptiveHeight autoplay>
         {images.map((image, index) => (
-          <div key={index}>
-            <div
+          <div
+            key={index}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "500px", // Max height for the carousel
+              overflow: "hidden",
+              borderRadius: "20px",
+              backgroundColor: "#f5f5f5", // Fallback background
+            }}
+          >
+            <Image
+              src={image}
+              alt={`Slide ${index + 1}`}
+              layout="intrinsic"
+              width={1920}
+              height={1080}
               style={{
-                height: "650px",
-                backgroundImage: `url(${image.src})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                borderRadius: "20px",
+                objectFit: "contain",
+                maxHeight: "100%",
+                maxWidth: "100%",
+                borderRadius: "10px" 
               }}
-            ></div>
+              sizes="(max-width: 768px) 100vw, 80vw"
+            />
           </div>
         ))}
       </Carousel>
